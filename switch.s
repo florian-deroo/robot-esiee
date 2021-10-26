9197f2315e5baa1f1303474908b5533056b3807d
Flush <59513489+florian-deroo@users.noreply.github.com>
2021-10-27 01:22:30 +0200

    Update switch.s

--- switch.s
+++ switch.s
@@ -1,15 +1,8 @@
         AREA    |.text|, CODE, READONLY
 
-; The GPIODATA register is the data register
 ; Les SWITCH sont sur la porte D
 GPIO_PORTD_BASE		EQU		0x40007000        
-; GPIO Port D (APB) base: 0x4000.7000 (p416 datasheet de lm3s9B92.pdf)
-
-; Digital enable register
-; To use the pin as a digital input or output, the corresponding GPIODEN bit must be set.
 GPIO_O_DEN			EQU     0x0000051C  ; GPIO Digital Enable (p437 datasheet de lm3s9B92.pdf)
-
-; Pul_up
 GPIO_I_PUR			EQU     0x00000510  ; GPIO Pull-Up (p432 datasheet de lm3s9B92.pdf)
 
 SWITCH_1			EQU     0x40
